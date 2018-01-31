07c420f1c081c389a6d6201359c1c267108ad78f
tankedshoe <tankedshoe@gmail.com>
2018-01-31 09:18:45 -0700

    Added the crime data stuff and the file controller.

--- DataStructures/Controller/FileController.hpp
+++ DataStructures/Controller/FileController.hpp
@@ -9,6 +9,21 @@
 #ifndef FileController_hpp
 #define FileController_hpp
 
+#include "../Data/CrimeData.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
 #include <stdio.h>
+
+using namespace std;
+
+class FileController
+{
+public:
+    static vector<CrimeData> readCrimeDataToVector(string filename);
+    
+};
 
 #endif /* FileController_hpp */

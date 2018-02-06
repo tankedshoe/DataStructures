44ce7247bd88b926c85e8ea653eb94b314322c04
tankedshoe <tankedshoe@gmail.com>
2018-02-06 08:29:19 -0700

    Created the array tester files.

---
+++ DataStructures/Testers/ArrayTester.cpp
@@ -0,0 +1,9 @@
+//
+//  ArrayTester.cpp
+//  DataStructures
+//
+//  Created by Heaps, Dane on 2/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "ArrayTester.hpp"
---
+++ DataStructures/Testers/ArrayTester.hpp
@@ -0,0 +1,17 @@
+//
+//  ArrayTester.hpp
+//  DataStructures
+//
+//  Created by Heaps, Dane on 2/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef ArrayTester_hpp
+#define ArrayTester_hpp
+
+#include <iostream>
+#include "../Model/Structures/Array.hpp"
+#include "../Controller/FileController.hpp"
+#include "../Controller/Tools/Timer.hpp"
+
+#endif /* ArrayTester_hpp */

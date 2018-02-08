0cc46d680c83799af064783c18e5bc1a1abb65be
tankedshoe <tankedshoe@gmail.com>
2018-02-08 09:44:17 -0700

    Moved my hpp code to my hpp file.

--- DataStructures/Model/Nodes/LinearNode.hpp
+++ DataStructures/Model/Nodes/LinearNode.hpp
@@ -11,4 +11,49 @@
 
 #include <stdio.h>
 
+#include "Node.hpp"
+
+template <class Type>
+class LinearNode : public Node<Type>
+{
+protected:
+    LinearNode<Type> * next;
+public:
+    LinearNode();
+    LinearNode(Type data);
+    LinearNode(Type data, LinearNode<Type> * next);
+    void setNextNode(LinearNode<Type> * next);
+    LinearNode<Type> * getNextNode();
+};
+
+template <class Type>
+LinearNode<Type> :: LinearNode() : Node<Type>()
+{
+    this->next = nullptr;
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data) : Node<Type>(data)
+{
+    this->next = nullptr;
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data, LinearNode<Type> * next) : Node<Type>(data)
+{
+    this->next = next;
+}
+
+template <class Type>
+void LinearNode<Type> :: setNextNode(LinearNode<Type> * nextNodePointer)
+{
+    this->next = nextNodePointer;
+}
+
+template <class Type>
+LinearNode<Type> * LinearNode<Type> :: getNextNode()
+{
+    return next;
+}
+
 #endif /* LinearNode_hpp */

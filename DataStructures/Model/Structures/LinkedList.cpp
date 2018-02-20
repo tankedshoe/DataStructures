313fabe3b1ec3cc82f1c13b77b8f942d2404a4e6
tankedshoe <tankedshoe@gmail.com>
2018-02-20 08:37:52 -0700

    Put all the hpp code in its right file, as well as the #includes.

--- DataStructures/Model/Structures/LinkedList.cpp
+++ DataStructures/Model/Structures/LinkedList.cpp
@@ -13,37 +13,4 @@ using namespace std; //Used for keyword access. Use this to make sure your point
 #ifndef LinkedList_hpp
 #define LinkedList_hpp
 
-template <class Type>
-class LinkedList: public List<Type>
-{
-protected:
-    LinearNode<Type> * front;
-    LinearNode<Type> * end;
-public:
-    //Constructors
-    LinkedList();
-    
-    //Destructor
-    virtual ~LinkedList(); //Virtual so it can be overridden.
-    
-    //Helper methods
-    int getSize() const;
-    LinearNode<Type> * getFront();
-    LinearNode<Type> * getEnd();
-    
-    //Structure Methods
-    void add(Type item);
-    void addAtIndex(int index, Type item);
-    Type getFromIndex(int index);
-    Type remove(int index);
-//    Type setAtIndex(int index, Type item);
-//    bool contains(Type item);
-};
 
-template <class Type>
-LinkedList<Type> :: LinkedList()
-{
-    this->front = nullptr;
-    this->end = nullptr;
-    this->size = 0;
-}
--- DataStructures/Model/Structures/LinkedList.hpp
+++ DataStructures/Model/Structures/LinkedList.hpp
@@ -9,6 +9,44 @@
 #ifndef LinkedList_hpp
 #define LinkedList_hpp
 
-#include <stdio.h>
+#include <iostream>
+#include <assert.h>
+#include "AbstractBase.hpp"
+#include "LinearNode.hpp"
+
+template <class Type>
+class LinkedList: public List<Type>
+{
+protected:
+    LinearNode<Type> * front;
+    LinearNode<Type> * end;
+public:
+    //Constructors
+    LinkedList();
+    
+    //Destructor
+    virtual ~LinkedList(); //Virtual so it can be overridden.
+    
+    //Helper methods
+    int getSize() const;
+    LinearNode<Type> * getFront();
+    LinearNode<Type> * getEnd();
+    
+    //Structure Methods
+    void add(Type item);
+    void addAtIndex(int index, Type item);
+    Type getFromIndex(int index);
+    Type remove(int index);
+    //    Type setAtIndex(int index, Type item);
+    //    bool contains(Type item);
+};
+
+template <class Type>
+LinkedList<Type> :: LinkedList()
+{
+    this->front = nullptr;
+    this->end = nullptr;
+    this->size = 0;
+}
 
 #endif /* LinkedList_hpp */

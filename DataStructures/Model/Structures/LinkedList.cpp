5841eb99847254a187fc52cfa2be9038375b7947
tankedshoe <tankedshoe@gmail.com>
2018-02-20 08:15:10 -0700

    Created the LinkedList class and its preproccessor commands.

---
+++ DataStructures/Model/Structures/LinkedList.cpp
@@ -0,0 +1,14 @@
+//
+//  LinkedList.cpp
+//  DataStructures
+//
+//  Created by Heaps, Dane on 2/20/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "LinkedList.hpp"
+
+using namespace std; //Used for keyword access. Use this to make sure your pointers are pointing to the right things
+
+#ifndef LinkedList_hpp
+#define LinkedList_hpp
---
+++ DataStructures/Model/Structures/LinkedList.hpp
@@ -0,0 +1,14 @@
+//
+//  LinkedList.hpp
+//  DataStructures
+//
+//  Created by Heaps, Dane on 2/20/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef LinkedList_hpp
+#define LinkedList_hpp
+
+#include <stdio.h>
+
+#endif /* LinkedList_hpp */

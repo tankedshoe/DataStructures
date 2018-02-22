ad1e2b611c09dd2dc9e6c65e0694c63fa221b2cf
tankedshoe <tankedshoe@gmail.com>
2018-02-22 08:57:32 -0700

    Created a LinkedList tester class and added some template methods.

---
+++ DataStructures/Testers/LinkedListTester.cpp
@@ -0,0 +1,9 @@
+//
+//  LinkedListTester.cpp
+//  DataStructures
+//
+//  Created by Heaps, Dane on 2/22/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "LinkedListTester.hpp"
---
+++ DataStructures/Testers/LinkedListTester.hpp
@@ -0,0 +1,25 @@
+//
+//  LinkedListTester.hpp
+//  DataStructures
+//
+//  Created by Heaps, Dane on 2/22/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef LinkedListTester_hpp
+#define LinkedListTester_hpp
+
+#include <iostream>
+#include "../Model/Structures/Linear/LinkedList.hpp"
+#include "../Controller/FileController.hpp"
+#include "../Controller/Tools/Timer.hpp"
+#include "../Data/CrimeData.hpp"
+
+class LinkedListTester
+{
+public:
+    void testListBasics();
+    void testListWithData();
+};
+
+#endif /* LinkedListTester_hpp */

22d2abe43011815a940a775d0a5ec96079b4ba0d
tankedshoe <tankedshoe@gmail.com>
2018-02-22 09:19:58 -0700

    Created the testListWithData method in the cpp class.

--- DataStructures/Testers/LinkedListTester.cpp
+++ DataStructures/Testers/LinkedListTester.cpp
@@ -8,6 +8,8 @@
 
 #include "LinkedListTester.hpp"
 
+using namespace std;
+
 void LinkedListTester :: testListBasics()
 {
     LinkedList<int> numbers;
@@ -38,4 +40,24 @@ void LinkedListTester :: testListBasics()
     cout << numbers.getFront()->getData() << " Is is at the front of the list and should be 9" << endl;
     cout << numbers.getEnd()->getData() << " Is is at the end of the list and should be 32567" << endl;
     
+}
+
+void LinkedListTester :: testListWithData()
+{
+    Timer listTimer;
+    listTimer.startTimer();
+    LinkedList<CrimeData> crimes = FileController :: readDataToList("Users/dhea8398/Documents/datasucksures/DataStructures/DataStructures/Data/crime.csv");
+    listTimer.stopTimer();
+    cout << "This is how long it took to read the structures into our custom data structure" << endl;
+    listTimer.displayInformation();
+    
+    listTimer.resetTimer();
+    cout << "Here is how long it takes to access a random data value" << endl;
+    listTimer.startTimer();
+    int randomLocation = (rand() * rand()) % crimes.getSize());
+    cout << "The random index is " << randomLocation << endl;
+    double totalViolentRate = crimes.getFromIndex(randomLocation).getAllViolentRates();
+    listTimer.stopTimer();
+    cout << "The random crime rate stat is: " << totalViolentRate << " , and here is the time" << endl;
+    listTimer.displayInformation();
 }

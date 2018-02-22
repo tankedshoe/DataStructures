ac4561e105f9f7522b8655bf3bfe22f5f3746c37
tankedshoe <tankedshoe@gmail.com>
2018-02-22 09:09:25 -0700

    Created the testListBasics method in the tester cpp class.

--- DataStructures/Testers/LinkedListTester.cpp
+++ DataStructures/Testers/LinkedListTester.cpp
@@ -7,3 +7,35 @@
 //
 
 #include "LinkedListTester.hpp"
+
+void LinkedListTester :: testListBasics()
+{
+    LinkedList<int> numbers;
+    numbers.add(9);
+    cout << numbers.getFront()->getData() << " Is is at the front of the list and should be 9" << endl;
+    cout << numbers.getEnd()->getData() << " Is is at the end of the list and should be 9" << endl;
+    
+    numbers.add(0);
+    cout << numbers.getFront()->getData() << " Is is at the front of the list and should be 9" << endl;
+    cout << numbers.getEnd()->getData() << " Is is at the end of the list and should be 0" << endl;
+    
+    numbers.addAtIndex(1, 2);
+    numbers.add(324);
+    
+    cout << numbers.getFront()>getData() << " Is is at the front of the list and should be 9" << endl;
+    cout << numbers.getEnd()-> getData() << " Is is at the end of the list and should be 324" < endl;
+    
+    cout << "This loop should print 4 lines" << endl;
+    for (int index = 0; index < numbers.getSize(); index++)
+    {
+        cout << numbers.getFromIndex(index) << endl;
+    }
+    
+    numbers.remove(0);
+    numbers.add(32567);
+    numbers.addAtIndex(0, 2312);
+    
+    cout << numbers.getFront()->getData() << " Is is at the front of the list and should be 9" << endl;
+    cout << numbers.getEnd()->getData() << " Is is at the end of the list and should be 32567" << endl;
+    
+}

af222456e5894997db97255188b98fe86eb8c00c
tankedshoe <tankedshoe@gmail.com>
2018-02-26 08:57:18 -0700

    Built the the declaration section of the DoubleNode class.

--- DataStructures/Model/Nodes/DoubleNode.hpp
+++ DataStructures/Model/Nodes/DoubleNode.hpp
@@ -9,6 +9,24 @@
 #ifndef DoubleNode_hpp
 #define DoubleNode_hpp
 
-#include <stdio.h>
+#include "Node.hpp"
+
+template <class Type>
+class DoubleNode : public Node<Type>
+{
+protected:
+    DoubleNode<Type> * previous;
+    DoubleNode<Type> * next;
+public:
+    DoubleNode();
+    DoublNode(Type data);
+    DoubleNode(Type data, DoubleNode<Type> * previous, DoubleNode<Type> * next);
+    
+    DoubleNode<Type> * getPrevious();
+    DoubleNode<Type> * getNext();
+    
+    void setPrevious(DoubleNode<Type> * previous);
+    void setNext(DoubleNode<Type> * next);
+}
 
 #endif /* DoubleNode_hpp */

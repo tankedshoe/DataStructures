db2b0f2dc901653c5b27a6aa361a15e3fc82755d
tankedshoe <tankedshoe@gmail.com>
2018-02-26 09:43:43 -0700

    Added the add method.

--- DataStructures/Model/Structures/Linear/CircularList.hpp
+++ DataStructures/Model/Structures/Linear/CircularList.hpp
@@ -79,4 +79,22 @@ DoubleNode<Type> * CircularList<Type> :: findNode(int index)
     return nodeToFind;
 }
 
+template <class Type>
+void CircularList<Type> :: add(Type item)
+{
+    DoubleNode<Type> * addedNode;
+    if (this->size == 0)
+    {
+        addedNode = new DoubleNode<Type>(item);
+        this->front = addedNode;
+    }
+    else
+    {
+        addedNode = new DoubleNode<Type>(item, this->end, this->front);
+        this->send->setNext(addedNode);
+    }
+    this->end = addedNode;
+    this->size++;
+}
+
 #endif /* CircularList_hpp */

057504ee2a1e3b21ff636263bb3707e54d880287
tankedshoe <tankedshoe@gmail.com>
2018-02-26 09:31:20 -0700

    Built the declaration section for CircularList.

--- DataStructures/Model/Structures/Linear/CircularList.hpp
+++ DataStructures/Model/Structures/Linear/CircularList.hpp
@@ -9,6 +9,26 @@
 #ifndef CircularList_hpp
 #define CircularList_hpp
 
-#include <stdio.h>
+#include "List.hpp"
+#include "../../Nodes/DoubleNode.hpp"
+
+template <class Type>
+class CircularList : public List<Type>
+{
+private:
+    DoubleNode<Type> * front;
+    DoubleNode<Type> * end;
+    
+    DoubleNode<Type> * findNOde(int index);
+public:
+    CircularList();
+    ~CircularList();
+    
+    void add(Type item);
+    void addAtIndex(int index, Type item);
+    Type remove(int index);
+    Type getFromIndex(int index);
+    int getSize() const;
+}
 
 #endif /* CircularList_hpp */

c8e2c88e588d1406cdc37bddc9ee79a00b17b140
tankedshoe <tankedshoe@gmail.com>
2018-03-19 09:20:51 -0600

    Finished addVertex.

--- DataStructures/Model/Structures/NonLinear/Graph.hpp
+++ DataStructures/Model/Structures/NonLinear/Graph.hpp
@@ -20,7 +20,7 @@ class Graph
 {
 private:
     static const int MAXIMUM = 20;
-    bool adjacentMatrix [MAXIMUM][MAXIMUM];
+    bool adjacencyMatrix [MAXIMUM][MAXIMUM];
     int weightCostMatrix [MAXIMUM][MAXIMUM];
     Type graphData[MAXIMUM];
     int vertexCount;
@@ -84,6 +84,22 @@ Type Graph<Type> :: operator[](int vertex) const
 {
     assert(vertex < vertexCount);
     return graphData[vertex];
+}
+
+template <class Type>
+void Graph<Type> :: addVertex(const Type& value)
+{
+    assert(vertexCount < MAXIMUM);
+    int newVertexNumber = vertexCount;
+    vertexCount++;
+    
+    for(int otherVertexNumber = 0; otherVertexNumber < vertexCount; otherVertexNumber++)
+    {
+        adjacencyMatrix[otherVertexNumber][newVertexNumber] = false;
+        adjacencyMatrix[newVertexNumber][otherVertexNumber] = false;
+    }
+    
+    graphData[newVertexNumber] = value;
 }
 
 
